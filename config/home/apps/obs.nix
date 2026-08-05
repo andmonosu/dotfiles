c2675e7b60183a3beb83443b01934184adbd2062
Andreu Montagut Osuna <andreuosuna@gmail.com>
2026-08-05 20:45:34 +0200

    Added new languajes

--- config/home/apps/obs.nix
+++ config/home/apps/obs.nix
@@ -1,5 +1,11 @@
-_: {
+{ pkgs, ... }: {
   programs.obs-studio = {
     enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vkcapture
+    ];
   };
 }
--- config/home/packages/devTools/default.nix
+++ config/home/packages/devTools/default.nix
@@ -6,6 +6,8 @@
     ./nixLang.nix
     ./web.nix
     ./docker.nix
+    ./xml.nix
+    ./rust.nix
   ];
 
   home.packages = with pkgs; [
